ebadbe729300c70c7e6653896ad0ac367e638263
AmirTheScientist <aainfoira@gmail.com>
2024-03-14 17:45:44 +0330

    speed limit downloader added

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask,jsonify
-
 from flask import Flask, request, jsonify
 import os,hashlib,secrets
 import threading
-from download_function import download_file_with_progress 
+from download_function import download_file_with_progress ,download_file_with_progress_limited
 
 def IDGenerator(filedest:str):
     _id=secrets.token_hex(16)
     if _id in Files_inprogress.keys():
-        if Files_inprogress[id]==0:# is finished
-            del Files_inprogress[id]
+        if Files_inprogress[_id]["dl"]==0:# is finished
+            del Files_inprogress[_id]
             return _id
         else:
             return IDGenerator(filedest)
@@ -22,6 +21,7 @@ def download():
     """sample of json input {'url':'http://sample.com','filesize':'100'} filesize must be in MB"""
     url=request.json.get('url')
     filesize=request.json.get('filesize')
+    chunksize=request.json.get('chunksize')
     if not url:
         return jsonify({'error': 'URL not provided'}), 400
     filename =os.path.basename(url)
@@ -29,11 +29,12 @@ def download():
     id=IDGenerator(destination_path)
     Files_inprogress[id]={'dl':filesize,'filename':filename}
 
-    # Use threading to download the file asynchronously
-    download_thread =threading.Thread(target=download_file_with_progress, args=(url, destination_path))
+    #threading to download the file asynchronously
+    download_thread =threading.Thread(target=download_file_with_progress, args=(url,destination_path,id,int(chunksize)))
     download_thread.start()
+    #Files_inprogress[id]["tr"]=download_thread
 
-    return jsonify({'message':'Download started','filename':filename}), 200
+    return jsonify({'message':'Download started','filename':filename,"ActionID":id}), 200
 
 @app.route('/progress/<ActionID>')
 def progress(ActionID):
@@ -45,8 +46,10 @@ def progress(ActionID):
         logging.error("Error on selecting file in progress maybe it is finished & removed. Error: "+str(ex))
         return jsonify("Error on selecting file in progress maybe it is finished & removed.")
     if os.path.exists(destination_path):
-        expected_file_size=Files_inprogress[Files_inprogress[ActionID]["dl"]]
+        expected_file_size=Files_inprogress[ActionID]["dl"]
         if expected_file_size:
+            if expected_file_size==0:
+                return jsonify({'progress': "100"})
             downloaded_file_size=round(float(os.path.getsize(destination_path)/(1024*1024)),2) #convert bytes to MB
             progress_percent =(downloaded_file_size / expected_file_size)*100
             return jsonify({'progress': progress_percent}), 200
@@ -55,13 +58,37 @@ def progress(ActionID):
 
 @app.route("/finish/<ActionID>",methods=["POST"])
 def finish_action(ActionID):
-    Files_inprogress[ActionID]=0
+    Files_inprogress[ActionID]["dl"]=0#we make it zero because the File might be deleted
+    print("finished registered",ActionID)
+    return jsonify("finished registered "+str(ActionID))
 @app.route('/selectall')
 def selectall():
     return jsonify(Files_inprogress)
-app.route('/delete/<actionid>')
+@app.route('/delete/<actionid>')
 def deleting(actionid):
     del Files_inprogress[actionid]
+
+@app.route('/limitdownload', methods=['POST'])
+def _download():
+    """sample of json input {'url':'http://sample.com','filesize':'100'} filesize must be in MB"""
+    url=request.json.get('url')
+    filesize=request.json.get('filesize')
+    chunksize=request.json.get('chunksize')
+    if not url:
+        return jsonify({'error': 'URL not provided'}), 400
+    filename =os.path.basename(url)
+    destination_path =f"downloads/{filename}"
+    id=IDGenerator(destination_path)
+    Files_inprogress[id]={'dl':filesize,'filename':filename}
+
+    #threading to download the file asynchronously
+    download_thread =threading.Thread(target=download_file_with_progress, args=(url,destination_path,id,int(chunksize)))
+    download_thread.start()
+    #Files_inprogress[id]["tr"]=download_thread
+
+    return jsonify({'message':'Download started','filename':filename,"ActionID":id}), 200
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
--- download_function.py
+++ download_function.py
@@ -1,19 +1,22 @@
-import requests
+import requests,logging
 from tqdm import tqdm  
-
-def download_file_with_progress(url, destination,chunksize=8192):
+import datetime,time
+import configparser
+conf=configparser.ConfigParser()
+conf.read("configs.ini")
+def download_file_with_progress(url, destination,action_id,chunksize=8192):
     try:
         response = requests.head(url)
         response.raise_for_status()
 
         content_length = response.headers.get('Content-Length')
         if content_length is None:
-            print("Cannot determine file size. Proceeding with download.")
+            print("Cannot determine file size.")
+            raise Exception("Cannot determine file size.")
         else:
             file_size = int(content_length)
             print(f"File size: {file_size / (1024 * 1024):.2f} MB")
 
-        # Perform the actual download with tqdm progress bar
         with requests.get(url, stream=True) as response:
             response.raise_for_status()
 
@@ -29,11 +32,50 @@ def download_file_with_progress(url, destination,chunksize=8192):
                     progress_bar.update(len(chunk))
 
         print(f"File downloaded successfully to {destination}")
+        logging.info(f"File downloaded successfully to {destination}")
+        requests.post(f"{conf.get('AppConfig','bind_url')}/finish/{action_id}")
+    except requests.exceptions.RequestException as ex:
+        print(f"Error downloading the file: {ex}")
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading the file: {e}")
+def download_file_with_progress_limited(url, destination,action_id,chunksize=8192):
+    try:
+        response = requests.head(url)
+        response.raise_for_status()
 
-# Example usage
-url = "https://example.com/sample_file.zip"
-destination_path = "path/to/save/sample_file.zip"
-download_file_with_progress(url, destination_path)
+        content_length=response.headers.get('Content-Length')
+        if content_length is None:
+            print("Cannot determine file size.")
+            raise Exception("Cannot determine file size.")
+        else:
+            file_size = int(content_length)
+            print(f"File size: {file_size / (1024 * 1024):.2f} MB")
+
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            LIMIT=0.5 #MBps
+            Limit_cycle=((LIMIT*1024)/(chunksize//1024))*60 # Howmany chunks should be downloaded in a minute
+            starttime=datetime.datetime.now()
+            _iterator=0
+            with open(destination, 'wb') as file,tqdm(
+                desc="Downloading",
+                total=file_size,
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as progress_bar:
+                for chunk in response.iter_content(chunk_size=chunksize):
+                    file.write(chunk)
+                    progress_bar.update(len(chunk))
+                    _iterator+=1
+                    if _iterator>Limit_cycle:
+                        executed_time=datetime.datetime.now()-starttime
+                        time.sleep(60-executed_time.seconds)
+                        print(f"Thread {destination} wainting to regulate speed")
+                        _iterator=0
+                        starttime=datetime.datetime.now()
+
+        print(f"File downloaded successfully to {destination}")
+        logging.info(f"File downloaded successfully to {destination}")
+        requests.post(f"{conf.get('AppConfig','bind_url')}/finish/{action_id}")
+    except requests.exceptions.RequestException as ex:
+        print(f"Error downloading the file: {ex}")
